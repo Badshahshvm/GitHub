d5880b4ca11e0ba34a472a67b1b6498dc6746601
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2024-09-03 15:59:11 +0530

    Third commit

--- Backend/controller/commit.js
+++ Backend/controller/commit.js
@@ -1,5 +1,35 @@
-const commitRepo = () => {
+const fs = require("fs").promises
+const { File } = require("buffer")
+const path = require("path")
+const { v4: uuidv4 } = require("uuid")
+const { detectLocale } = require("yargs")
+
+const commitRepo = async (message) => {
               console.log("commit repo is called")
+              const repoPath = path.resolve(process.cwd(), ".AddGit");
+              const staggedPath = path.join(repoPath, "staging")
+              const commitPath = path.join(repoPath, "commits");
+              try {
+                            const commitId = uuidv4();
+                            const commitDir = path.join(commitPath, commitId)
+                            await fs.mkdir(commitDir, { recursive: true })
+
+                            const files = await fs.readdir(staggedPath);
+                            for (const file of files) {
+                                          await fs.copyFile(path.join(staggedPath, file), path.join(commitDir, file))
+                            }
+
+                            await fs.writeFile(path.join(commitDir, "commit.json"), JSON.stringify({
+                                          message, date: new Date().toISOString()
+                            }))
+
+
+
+                            console.log(`commit ${commitId}`)
+              }
+              catch (err) {
+                            console.error("error occur", err);
+              }
 }
 
 module.exports = { commitRepo }
--- Backend/index.js
+++ Backend/index.js
@@ -27,7 +27,9 @@ yargs(hideBin(process.argv)).command(
                                           describe: "Commit message",
                                           type: "string"
                             })
-}, commitRepo).command("push", "Push commits to S3", {}, pushRepo).command("pull", "Pull commits from S3", {}, pullRepo).command("revert<commitID", "Revert to a specific commit", (yargs) => {
+}, (argv) => {
+              commitRepo(argv.message)
+}).command("push", "Push commits to S3", {}, pushRepo).command("pull", "Pull commits from S3", {}, pullRepo).command("revert<commitID", "Revert to a specific commit", (yargs) => {
               yargs.positional("commitID",
                             {
                                           describe: "CommitID to revert to",
